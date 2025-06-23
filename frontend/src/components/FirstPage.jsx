adb0ae6bf3866bb02bff8ea7824b615fc4d8baf7
Hammad Malik <122615617+HammadMal@users.noreply.github.com>
2025-06-23 22:30:28 +0500

    First Text

--- frontend/src/components/FirstPage.jsx
+++ frontend/src/components/FirstPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { HeroSectionOne } from "./HeroSectionOne";
+
 
 export default function FirstPage() {
     return (
-        <h1>k</h1>
+
+        <HeroSectionOne />
+        
     );
 }
