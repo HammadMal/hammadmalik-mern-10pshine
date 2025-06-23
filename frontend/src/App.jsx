464ee6f1222a3376cfb2dc1b4b3b2db175496b1f
Hammad Malik <122615617+HammadMal@users.noreply.github.com>
2025-06-23 17:07:55 +0500

    main

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+import FirstPage from './components/FirstPage'
+
+function App() {
+
+  return (
+    <div className="App">
+      <FirstPage />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/FirstPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import DotBackgroundDemo from "./ui/DotBackgroundDemo";
+
+//landing page component
+
+
+export default function FirstPage() {
+    return (
+        <DotBackgroundDemo>
+
+        </DotBackgroundDemo>
+    );
+    }
