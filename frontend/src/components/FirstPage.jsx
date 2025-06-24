dc5264a5db8a15ab412f176e9a1db811aaf15a07
Hammad Malik <122615617+HammadMal@users.noreply.github.com>
2025-06-24 16:15:35 +0500

    SVG

--- frontend/src/components/FirstPage.jsx
+++ frontend/src/components/FirstPage.jsx
@@ -8,9 +8,25 @@ import Particles from './Particles';
 
 import Footer from "./Footer";
 
+import { GoogleGeminiEffect } from "./GoogleGeminiEffect";
+import { useScroll, useTransform } from "motion/react";
+
+
 // import { HeroSectionOne } from "./HeroSectionOne";
 
 export default function FirstPage() {
+    const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+    });
+
+    const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+    const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+    const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+    const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+    const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
     return (
         <>
             {/* Full-page background dot grid */}
@@ -48,7 +64,24 @@ export default function FirstPage() {
 
                 <HeroSectionOne />
 
+                <div
+      className="h-[400vh] bg-transparent w-full dark:border dark:border-black/[0.1] rounded-md relative pt-40 overflow-clip"
+      ref={ref}
+    >
+      <GoogleGeminiEffect
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+      />
+    </div>
+
                 <Footer />
+
+
 
                 
 
