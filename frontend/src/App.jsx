744f3bf6f29ce152afa3e7a9477150da9d1e2348
Hammad Malik <122615617+HammadMal@users.noreply.github.com>
2025-07-03 22:09:57 +0500

    SignIn and SignUp Pages completed. Routing left

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import './App.css'
 
 import SignUp from './components/SignUp'
 
+import SignIn from './components/SignIn'
+
 
 
 function App() {
 
   return (
-   <SignUp />
+   <SignIn />
   )
 }
 
