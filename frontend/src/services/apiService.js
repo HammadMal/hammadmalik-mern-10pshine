df7ee62249193ecb1be2dcc4c5c02a4b9bfcde48
Hammad Malik <122615617+HammadMal@users.noreply.github.com>
2025-09-04 23:14:16 +0500

    fixes

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,5 +1,5 @@
 // src/services/apiService.js
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://notehive.ddns.net';
 
 class ApiService {
   // Helper method for making authenticated requests
